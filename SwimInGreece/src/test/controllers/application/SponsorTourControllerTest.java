87ff0e9616eca48c52c4a0501aa758f6204b3223
zeroParanoie <andrea.tsilogiannis@gmail.com>
2024-06-11 22:59:17 +0200

    added test cases, fixed view for good. Need to write test cases

---
+++ SwimInGreece/src/test/controllers/application/SponsorTourControllerTest.java
@@ -0,0 +1,16 @@
+package controllers.application;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SponsorTourControllerTest {
+
+    @Test
+    void saveTour() {
+    }
+
+    @Test
+    void saveSwim() {
+    }
+}
---
+++ SwimInGreece/src/test/engClasses/DAO/LogWriterDAOTest.java
@@ -0,0 +1,12 @@
+package engClasses.DAO;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogWriterDAOTest {
+
+    @Test
+    void updateLog() {
+    }
+}
